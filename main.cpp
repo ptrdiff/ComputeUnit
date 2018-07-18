6902b3e9ec201c1ebd24a5df51768ffe5b159abd
Timur <ptrdiff_t@gmail.com>
2018-07-18 13:14:00 +0300

    Add logger handler.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 ﻿#include <QtCore/QCoreApplication>
-
+#include <QDateTime>
 #include "Executor/Executor.h"
 
 #include <iostream>
@@ -8,26 +8,53 @@ Q_DECLARE_METATYPE(std::function<void()>)
 
 void initialise()
 {
-    qRegisterMetaType<std::function<void()>>("myLyambda");
-    qRegisterMetaType<QVector<double>>("myQVectorDouble");
-    qInfo() << "Meta tpes registered";
+  qRegisterMetaType<std::function<void()>>("myLyambda");
+  qRegisterMetaType<QVector<double>>("myQVectorDouble");
+  qInfo() << "Meta tpes registered";
 }
+
+void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+  QByteArray localMsg = msg.toLocal8Bit();
+  QString msgType;
+  switch (type)
+  {
+    case QtDebugMsg: msgType = "Debug"; break;
+    case QtInfoMsg: msgType = "Info"; break;
+    case QtWarningMsg: msgType = "Warning"; break;
+    case QtCriticalMsg: msgType = "Critical"; break;
+    case QtFatalMsg: msgType = "Fatal"; break;
+  }
+  QDateTime now = QDateTime::currentDateTime();
+  now.setOffsetFromUtc(now.offsetFromUtc());
+  std::cerr << QString("%1 %6: %2 (%3, %4:%5)\n").arg(
+      now.toString(Qt::ISODateWithMs),
+      localMsg.constData(),
+      context.function,
+      context.file,
+      QString::number(context.line),
+      msgType
+  ).toStdString();
+} // TODO add feature to change stream output.
 
 int main(int argc, char *argv[])
 {
-    try {
-        QCoreApplication a(argc, argv);
+  try
+  {
+    qInstallMessageHandler(myMessageOutput);
+    QCoreApplication a(argc, argv);
 
-        initialise();
+    initialise();
 
-        //Executor executor("172.27.221.60", 59002, 9090);
-        Executor executor("localhost", 59002, "localhost", 9090);
+    //Executor executor("172.27.221.60", 59002, 9090);
+    Executor executor("localhost", 59002, "localhost", 9090);
 
-        return a.exec();
-    }
-    catch (std::exception& exp) {
-        std::cout << exp.what() << '\n';
-        throw exp;
-    }
+    return a.exec();
+  }
+  catch (std::exception &exp)
+  {
+    std::cout << exp.what() << '\n';
+    throw exp;
+  }
 
 }
